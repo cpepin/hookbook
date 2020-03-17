264fdad0ebfde1d7ea1431807a32d49ad7d891b1
cpepin <cam@cpepin.com>
2020-03-17 07:39:27 -0400

    Adding explicit return type for useStateWithGetter

--- hooks/useStateWithGetter/index.ts
+++ hooks/useStateWithGetter/index.ts
@@ -1,30 +1,38 @@
-
-import { useCallback, useState, useRef, Dispatch, SetStateAction } from 'react';
+import { useCallback, useState, useRef, Dispatch, SetStateAction } from 'react'
 
 /**
  * useState function with a getter.
- * 
+ *
  * @param initialState
  */
-function useStateWithGetter<T>(initialState: T | (() => T)) {
-  const [state, setState] = useState(typeof initialState === 'function' ? (initialState as () => T)() : initialState);
-  const ref = useRef<T>(state);
-  ref.current = state;
+function useStateWithGetter<T>(
+  initialState: T | (() => T)
+): [T, Dispatch<SetStateAction<T>>, () => T] {
+  const [state, setState] = useState(
+    typeof initialState === 'function'
+      ? (initialState as () => T)()
+      : initialState
+  )
+  const ref = useRef<T>(state)
+  ref.current = state
 
-  const set = useCallback<Dispatch<SetStateAction<T>>>((setStateAction) => {
-    if (typeof setStateAction === 'function') {
-      ref.current = (setStateAction as (prevState: T) => T)(state);
-    }
+  const set = useCallback<Dispatch<SetStateAction<T>>>(
+    setStateAction => {
+      if (typeof setStateAction === 'function') {
+        ref.current = (setStateAction as (prevState: T) => T)(state)
+      }
 
-    ref.current = setStateAction as T;
-    setState(ref.current);
-  }, [setState]);
+      ref.current = setStateAction as T
+      setState(ref.current)
+    },
+    [setState]
+  )
 
   const get = useCallback(() => {
-    return ref.current;
-  }, []);
+    return ref.current
+  }, [])
 
-  return [state, set, get];
+  return [state, set, get]
 }
 
-export default useStateWithGetter;
+export default useStateWithGetter
